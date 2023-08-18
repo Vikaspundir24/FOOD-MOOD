a58af00d2f5cc1b185e762c7f5602961fa0c150e
vikas pundir <vikaspundiruser@gmail.com>
2023-08-18 11:36:41 +0530

    fixed many bugs and also added an alert if limit  reached

--- script.js
+++ script.js
@@ -2,39 +2,40 @@
 // UNIQUE KEY 2 = apiKey=90edaf4613584af6a5651894feb4ce99
 // UNIQUE KEY 3 = apiKey=3e55c57d41b04ecd9354ee0b5095685b
 
-
-
 const input = document.querySelector(".input");
 const clickBtn = document.querySelector(".click-btn");
-const searchResult = document.querySelector(".search-result")
-const result = document.querySelector(".result")
-const resultContainer = document.querySelectorAll(".result-container")
+const searchResult = document.querySelector(".search-result");
+const result = document.querySelector(".result");
+const resultContainer = document.querySelectorAll(".result-container") 
 const detailedView = document.querySelector(".detailed-view");
-const goBack = document.querySelector(".goBack")
-const loaderContainer = document.querySelector('.loader-container');
-
+const goBack = document.querySelector(".goBack");
+const loaderContainer = document.querySelector(".loader-container");
 
 /* RANDOM FOOD IN POPULAR SECTION */
 
-
-const popuFood = fetch("https://api.spoonacular.com/recipes/random?number=6&apiKey=3e55c57d41b04ecd9354ee0b5095685b")
-    .then(response => {
-        return response.json()
+const popuFood = fetch(
+        "https://api.spoonacular.com/recipes/random?number=6&apiKey=3e55c57d41b04ecd9354ee0b5095685b"
+    )
+    .then((response) => {
+        return response.json();
     })
-    .then(data => {
-        
-        const finalData = data
-        console.log(finalData)
-
+    .then((data) => {
+        const finalData = data;
+        console.log(finalData);
+        if(finalData.status === "failure"){
+           alert("TODAY'S LIMIT REACHED")
+        }
         for (i = 0; i < 6; i++) {
             let sourceUrl = finalData.recipes[i].sourceUrl;
-            document.getElementById("img" + (i + 1)).innerHTML = `  <a href= "${sourceUrl}" target ="tab"> <img src="${finalData.recipes[i].image}"></img>  </a>`;
+            document.getElementById(
+                "img" + (i + 1)
+            ).innerHTML = `  <a href= "${sourceUrl}" target ="tab"> <img src="${finalData.recipes[i].image}"></img>  </a>`;
         }
         for (i = 0; i < 6; i++) {
-            document.getElementById("d" + (i + 1)).innerText = finalData.recipes[i].title;
+            document.getElementById("d" + (i + 1)).innerText =
+                finalData.recipes[i].title;
         }
-    })
-
+    });
 
 /* SEARCH RESULT  */
 /* MAIN CODE FROM HERE */
@@ -47,79 +48,73 @@ clickBtn.addEventListener("click", () => {
         resultFound();
         input.value = "";
     } else {
-        alert("ENTER A  DISH ")
+        alert("ENTER A  DISH ");
         input.value = "";
     }
-
-
-})
-
+});
 
 //RESULT OF SEARCH ITEM
 
 function resultFound() {
-   
-    fetch(`https://api.spoonacular.com/recipes/complexSearch?query=${input.value}&number=100&apiKey=3e55c57d41b04ecd9354ee0b5095685b`)
-        .then(response => {
-            return response.json()
+    fetch(
+            `https://api.spoonacular.com/recipes/complexSearch?query=${input.value}&number=24&apiKey=3e55c57d41b04ecd9354ee0b5095685b`
+        )
+        .then((response) => {
+            return response.json();
             const recipeData = response.json();
         })
-        .then(data => {
-           
-            const recipeData = data
+        .then((data) => {
+            const recipeData = data;
             console.log(recipeData);
             let l = recipeData.results.length;
             if (recipeData.totalResults === 0) {
-                alert("NO MEALS FOUND")
+                alert("NO MEALS FOUND");
             } else {
                 searchResult.style.display = "flex";
                 result.style.display = "flex";
 
-                result.innerHTML = recipeData.results.map(
-                    results => `<div class = "result-container" data-mealID= "${results.id}"  >
+                result.innerHTML = recipeData.results
+                    .map(
+                        (
+                            results
+                        ) => `<div class = "result-container" data-mealID= "${results.id}"  >
                     <img src = "${results.image}">
                     <h2>"${results.title}"</h2>
                      </div>`
-                ).join('')
-
+                    )
+                    .join("");
             }
-
         })
-        .catch(err => alert("SOMETHING WENT WRONG"))
+        .catch((err) => alert("SOMETHING WENT WRONG"));
 }
-
 
 // GET DETAILS OF CLICKED CARD-CONTAINER DISH
 
-result.addEventListener('click', e => {
-    const mealInfo = e.path.find(item => {
-        if (item.classList) {
-            return item.classList.contains("result-container");
-        } else {
-            return false;
-        }
-    });
 
-
-    if (mealInfo) {
-        const mealID = mealInfo.getAttribute("data-mealID")
+result.addEventListener('click', (e) => {
+    if(e.target.parentElement.classList.contains("result-container")){
+        console.log("yes")
+        const mealID = e.target.parentElement.getAttribute("data-mealid")
         console.log(mealID)
-        getMealById(mealID);
+        result.style.display = "none";
+        detailedView.style.display = "flex";
+        getMealById(mealID)
     }
+   
+});
 
-    result.style.display = "none";
-    detailedView.style.display = "flex";
-})
 
 
 //MEAL BY ID FOR DETAILED VIEW
 
 function getMealById(mealID) {
-    fetch(`https://api.spoonacular.com/recipes/${mealID}/information?apiKey=3e55c57d41b04ecd9354ee0b5095685b`)
+    fetch(
+            `https://api.spoonacular.com/recipes/${mealID}/information?apiKey=3e55c57d41b04ecd9354ee0b5095685b`
+        )
         .then((res) => res.json())
         .then((data) => {
             const mealData = data;
-            console.log(mealData)
+            console.log(mealData);
 
             detailedView.innerHTML = `
                       <div class="headline">
@@ -134,27 +129,24 @@ function getMealById(mealID) {
             <div class="bottom">
                  <h2>Procedure</h2>
                 <p>${mealData.instructions}</p>
-            </div>`
-        })
-
-        
+            </div>`;
+        });
 }
 
-function back(){
-    console.log("han")
+function back() {
+    console.log("han");
     detailedView.style.display = "none";
     result.style.display = "flex";
-
 }
-  
-window.addEventListener('load', () => {
-    loaderContainer.style.display = 'none';
+
+window.addEventListener("load", () => {
+    loaderContainer.style.display = "none";
 });
 
 const displayLoading = () => {
-    loaderContainer.style.display = 'block';
+    loaderContainer.style.display = "block";
 };
 
 const hideLoading = () => {
-    loaderContainer.style.display = 'none';
+    loaderContainer.style.display = "none";
 };
